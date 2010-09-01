a464e947049d886cfa7eb3fba414709a45ba00c4
Philipp Bosch <pb@philippbosch.de>
2010-09-01 11:45:14 +0200

    Adapted to pb.io design. Hovering link bar added. Copy-to-clipboard added.

--- media/js/master.js
+++ media/js/master.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    var zoomLink = $('<a href="#"></a>');
+    var zoomLink = $('a.zoom');
     zoomLink.click(function(e) {
         e.preventDefault();
-        var $img = $(this).find('img');
+        var $img = $('.image img');
         if (!$img.data('originalLoaded')) {
             $img.attr('src', $img.attr('longdesc'));
             $img.data('originalLoaded', true);
@@ -10,6 +10,33 @@ $(document).ready(function() {
             });
         }
         $img.parents('.image').toggleClass('small large');
+        $(this).toggleClass('in out');
     });
-    $('.image.scalable img').wrap(zoomLink);
+    $('.edit, .delete').click(function(e) {
+        e.preventDefault();
+        var win = window.open($(this).attr('href') + '?_popup=1', 'img_edit_delete', 'height=500,width=800,resizable=yes,scrollbars=yes');
+        win.focus();
+        return false;
+    });
+    $('.links').append('<div class="title"></div>');
+    $('.links a').hover(function() {
+        $(this).parents('.links').find('.title').text($(this).find('span').text());
+    }, function() {
+        if ($(this).parents('.links').find('.title').text() == $(this).find('span').text()) {
+            $(this).parents('.links').find('.title').text('');
+        }
+    });
+    
+    ZeroClipboard.setMoviePath(settings.MEDIA_URL + 'js/lib/zeroclipboard/ZeroClipboard10.swf');
+    var clip = new ZeroClipboard.Client();
+    clip.setText('lorem ipsum FTW!');
+    clip.glue($('.links a.copy').get(0));
+    clip.addEventListener('onMouseOver', function() {
+        $('.image').addClass('hover');
+        $('a.copy').trigger('mouseover');
+    });
+    clip.addEventListener('onMouseOut', function() {
+        $('.image').removeClass('hover');
+        $('a.copy').trigger('mouseout');
+    });
 });
